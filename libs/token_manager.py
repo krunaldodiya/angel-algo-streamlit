940374ed96cae1f2fa9698a909885ab1edd83375
Krunal Dodiya <kunal.dodiya1@gmail.com>
2024-04-08 10:11:17 +0530

    done

--- libs/token_manager.py
+++ libs/token_manager.py
@@ -42,9 +42,7 @@ def validate_token_manager(client_id, totp_key, mpin, api_key, api_secret, redir
             redirect_url=redirect_url,
         )
 
-        session = token_manager.get_session()
-
-        profile = token_manager.http_client.getProfile(refreshToken=session["refreshToken"])
+        profile = token_manager.http_client.getProfile(refreshToken=token_manager.session["data"]["refreshToken"])
 
         if profile['status']:
             return True
